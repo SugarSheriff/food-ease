ce85a720c29f043b6a063ff67abf4019e497eb2d
Tbroskii <103056212+Tbroskii@users.noreply.github.com>
2023-07-24 21:22:45 -0500

    commented out api calls
    commented out api calls so that we can work with out using them until they are needed

--- assets/script.js
+++ assets/script.js
@@ -32,16 +32,23 @@
 
 
 //API call for food commented out so that we dont use api calls while working out rest of page
-var query = 'italian wedding soup'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/recipe?query=' + query,
-    headers: { 'X-Api-Key': '09nNnsjpkRmZ89Iq9Ihe7g==AjT05Ms9KWvG9vkG'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+// var query = 'italian wedding soup'
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/recipe?query=' + query,
+//     headers: { 'X-Api-Key': '09nNnsjpkRmZ89Iq9Ihe7g==AjT05Ms9KWvG9vkG'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
+
+//Dropdown click functionality on saved recipes
+$(document).ready(function() {
+    $('.recipe-title').click(function() {
+      $(this).siblings('.recipe-content').slideToggle();
+    });
+  });
