8f2d5fd9d865b66537205889e9e73a898a039f18
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-07-26 21:32:44 -0500

    Added styling to main-page

--- assets/script.js
+++ assets/script.js
@@ -29,6 +29,14 @@
 //     }
 // });
 
+//Dropdown click for burger//
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active')
+})
+
 //Dropdown click functionality on saved recipes
 $(document).ready(function() {
     $('.recipe-title').click(function() {
