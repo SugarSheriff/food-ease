455ad645752e3eeeb3ae06eca1a910999d63f25d
Tbroskii <103056212+Tbroskii@users.noreply.github.com>
2023-07-26 22:36:30 -0500

    updated html to make sure it is working again

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,15 @@
 $(function(){
 var searchBar = $('#main-search');
-var savedData;
 
 function displayFoodResults(item){
+  console.log(item);
   window.location.href = 'results.html';// switch to results page
   var data = JSON.parse(localStorage.getItem(`${item}Result`));//use template literal to parse out localstorage based on firstword
-  console.log(data[0].title);//log for testing
+  console.log(data[0].instructions);//log for testing
 }
 
-
 //food button click functionality
-function foodButton(query){
+function foodButton(){
 //API call for food commented out so that we dont use api calls while working out rest of page
   var query = searchBar.val();
   $.ajax({
@@ -32,33 +31,35 @@ function foodButton(query){
   }
 
 //drink button click functionality
-function drinkButton(data){
+function drinkButton(){
     var name = searchBar.val();
+
     $.ajax({
         method: 'GET',
         url: 'https://api.api-ninjas.com/v1/cocktail?name=' + name,
         headers: { 'X-Api-Key': '09nNnsjpkRmZ89Iq9Ihe7g==AjT05Ms9KWvG9vkG'},
         contentType: 'application/json',
-        success: function(data) { 
+        success: function(result) { 
           localStorage.setItem('drinkResult', JSON.stringify(result));
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
     });
-    setTimeout(function(){//using timeout to make sure and get data from api before the page is redirected
+
+   setTimeout(function(){//using timeout to make sure and get data from api before the page is redirected
       displayFoodResults('drink');
     }, 1000);
   }
 
 
-//Dropdown click for burger//
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
+// //Dropdown click for burger//
+// const burgerIcon = document.querySelector('#burger');
+// const navbarMenu = document.querySelector('#nav-links');
 
-burgerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active')
-})
+// burgerIcon.addEventListener('click', () => {
+//   navbarMenu.classList.toggle('is-active')
+// })
 
 //Dropdown click functionality on saved recipes
 $(document).ready(function() {
